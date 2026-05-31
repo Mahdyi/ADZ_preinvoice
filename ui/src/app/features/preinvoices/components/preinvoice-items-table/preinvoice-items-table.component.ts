b2dd74a410bda06db21741f8e8b12732d5444c01
Mahdyi <mahdi.nouroozi@gmail.com>
2026-05-31 15:16:31 +0330

    Button removed and a few css reworks

--- ui/src/app/features/preinvoices/components/preinvoice-items-table/preinvoice-items-table.component.ts
+++ ui/src/app/features/preinvoices/components/preinvoice-items-table/preinvoice-items-table.component.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { ButtonModule } from 'primeng/button';
 import { EquipmentCatalogItem } from '../../../equipment/models/equipment.model';
 import { InvoiceRow } from '../../models/preinvoice.model';
 import { PreinvoiceItemRowComponent } from '../preinvoice-item-row/preinvoice-item-row.component';
@@ -13,7 +12,7 @@ export interface EquipmentSelectedEvent {
 @Component({
   selector: 'app-preinvoice-items-table',
   standalone: true,
-  imports: [CommonModule, ButtonModule, PreinvoiceItemRowComponent],
+  imports: [CommonModule, PreinvoiceItemRowComponent],
   templateUrl: './preinvoice-items-table.component.html',
   styleUrl: './preinvoice-items-table.component.css',
 })
@@ -21,7 +20,6 @@ export class PreinvoiceItemsTableComponent {
   @Input() rows: InvoiceRow[] = [];
   @Input() rowTotal: (row: InvoiceRow) => number = () => 0;
 
-  @Output() newEquipmentRequested = new EventEmitter<void>();
   @Output() equipmentSearch = new EventEmitter<InvoiceRow>();
   @Output() equipmentSelected = new EventEmitter<EquipmentSelectedEvent>();
   @Output() createEquipment = new EventEmitter<InvoiceRow>();
